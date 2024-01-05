5a566556b799953a5757b579598591b8b4df224c
probeshnath <probeshn@gmail.com>
2024-01-06 00:14:17 +0600

    target the class name and button

--- weather.js
+++ weather.js
@@ -1,1 +1,7 @@
-console.log("Hello world")
+const temp_button = document.getElementById("temperature_button")
+const country_name = document.getElementsByClassName("country_name")
+const temparature = document.getElementsByClassName("temparature")
+
+temp_button.addEventListener("click", function(){
+    console.log("clicked")
+})

8f3dba57c08339e4d3a7f2fb18ad371ecfcbc1c4
probeshnath <probeshn@gmail.com>
2024-01-06 14:08:48 +0600

    weather app

--- weather.js
+++ weather.js
@@ -3,11 +3,7 @@ const cityName = document.getElementById("cityName")
 const country_name = document.getElementById("country_name")
 const temparature = document.getElementById("temparature")
 
-// temp_button.addEventListener("click", function(){
-//     console.log("clicked")
-//    const data = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country_name}&appid=e84a7695a9071048e9e577e7abdb4dbf`) 
-//    console.log(country_name)
-// })
+
 
 temp_button.addEventListener("click", function(){
     const cityValue = cityName.value;
@@ -18,20 +14,21 @@ temp_button.addEventListener("click", function(){
         .then(response => response.json())
         .then(data => {
             // Update the DOM by fetching data
-            console.log(data)
+            // console.log(data)
             const country = data?.sys?.country;
-            console.log("country: " , country)
+            // console.log("country: " , country)
             const tem_celceus = data?.main?.temp
-            console.log("tem F: ",tem_celceus)
-            const  Celsius = ( tem_celceus - 32 ) * (9/5)
-            console.log("Dgree", Celsius)
+            // console.log("tem F: ",tem_celceus)
+            // const  Celsius = ( tem_celceus - 32 ) * (9/5)
+            const  Celsius = tem_celceus - 273.15 
+
+            // console.log("Dgree", Celsius)
 
 
 
-
-            // You can add additional information extraction and updates here
+            //  updates here
             country_name.textContent  = country;
-            temparature.textContent  = tem_celceus;
+            temparature.textContent  = Celsius.toFixed(2) + " °C";
         })
         .catch(error => {
             console.error("Error fetching data:", error);
